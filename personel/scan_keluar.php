b1044b40dac496ec1b550c3006340bc78503ce33
Anggairm <9i.mairlanggaa.81@gmail.com>
2025-08-13 16:07:40 +0700

    rekap+kelola personel+login

--- personel/scan_keluar.php
+++ personel/scan_keluar.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION['personel_id'])) {
 
         body {
             font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
-            background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+            background: linear-gradient(135deg,rgb(90, 119, 248) 0%,rgb(32, 65, 255) 100%);
             min-height: 100vh;
             display: flex;
             align-items: center;
